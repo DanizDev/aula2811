ede2ecb0a4a23b506236090fcd667008fba115dd
DanizDev <jose.daniel.alicrim9@hotmail.com>
2024-11-28 21:22:53 -0300

    sripts aula 28/11

---
+++ script.js
@@ -0,0 +1,19 @@
+var numero1 = parseFloat(prompt("Digite o primeiro numero: "));
+var numero2 = parseFloat(prompt("Digite o segundo numero: "));
+
+if(numero1 > numero2){
+
+    alert("O maior numero é : " + numero1);
+
+}else{
+
+    alert("O maior numero é : " + numero2);
+
+}
+
+
+
+
+
+
+
